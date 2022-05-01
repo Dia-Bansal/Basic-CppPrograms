5960dfe8ff61b4a0d71c23f3d5205cf1ad7ef627
Dia-Bansal <78194499+Dia-Bansal@users.noreply.github.com>
2022-05-01 13:12:25 +0530

    print Logical Operators program

---
+++ LogicalOperators.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cin>>a>>b;
+    cout<<((a>10)&&(b<5))<<endl;
+    cout<<((a>10)||(b<5))<<endl;
+    cout<<(!a)<<endl;
+    cout<<(!b)<<endl;
+    return 0;
+    
+}
